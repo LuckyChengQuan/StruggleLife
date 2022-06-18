72fd0f30f7a9b07172c3a4c3d2800b3cdc99a59b
LuckyChengQuan <107754207+LuckyChengQuan@users.noreply.github.com>
2022-06-18 19:15:30 +0800

    Add files via upload

---
+++ demojs.js
@@ -0,0 +1,12 @@
+// alert("error!");
+// var a = 3;
+// alert(a);
+// a="abc";
+// alert(a);
+// var num1 = 1;
+// var num2 = 1.2;
+// var num3 = NaN;
+//
+//     document.write(num1+typeof (num1)+"<br>");
+//     document.write(num2+"<br>");
+//     document.write(num3+"<br>");
